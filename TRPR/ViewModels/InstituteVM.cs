550cfdb97c629855e2368ea4d03e87dbcfc27ae9
davileemaitre <45205184+davileemaitre@users.noreply.github.com>
2019-03-25 14:36:57 -0400

    Create InstituteVM.cs

---
+++ TRPR/ViewModels/InstituteVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TRPR.ViewModels
+{
+    public class InstituteVM
+    {
+        public int InstituteID { get; set; }
+        public string InstName { get; set; }
+    }
+}
