502c76c8199c68f3e0b3227701b8b84a63ebe7a2
Davi-lee <davilee.maitre@gmail.com>
2019-04-02 20:34:05 -0400

    Update TRPRContextModelSnapshot.cs

--- TRPR/Data/TRPRMigrations/TRPRContextModelSnapshot.cs
+++ TRPR/Data/TRPRMigrations/TRPRContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace TRPR.Data.TRPRMigrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasDefaultSchema("TRPR")
-                .HasAnnotation("ProductVersion", "2.1.1-rtm-30846")
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
