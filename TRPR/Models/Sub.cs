6c1b860afcd795362238f1e5467e2461952f0885
davileemaitre <45205184+davileemaitre@users.noreply.github.com>
2019-04-17 14:36:03 -0400

    Create Sub.cs

---
+++ TRPR/Models/Sub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TRPR.Models
+{
+    public class Sub
+    {
+        public int Id { get; set; }
+
+        [StringLength(127)]
+        public string Name { get; set; }
+
+        [StringLength(512)]
+        public string PushEndpoint { get; set; }
+
+        [StringLength(512)]
+        public string PushP256DH { get; set; }
+
+        [StringLength(512)]
+        public string PushAuth { get; set; }
+
+        public int? ResearcherID { get; set; }
+        public Researcher Researcher { get; set; }
+
+    }
+}
